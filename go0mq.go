ae30dbc804619d5a364f768527906bfa8947410b
AbhishekKr <abhikumar163@gmail.com>
2014-02-12 09:04:12 +0530

    made zeromq also use levigoNS and levigoTSDS

--- go0mq.go
+++ go0mq.go
@@ -3,25 +3,64 @@ package goshare
 import (
   "fmt"
   "runtime"
+  "strings"
+  "strconv"
 
-  "github.com/abhishekkr/goshare/zeromq"
+  abkzeromq "github.com/abhishekkr/goshare/zeromq"
 )
 
-func ReadKey(key string) string{
-  return GetVal(key)
-}
 
-func PushKey(key string, val string) bool{
-  return PushKeyVal(key, val)
-}
+func goShareZmqRep(req_port int, rep_port int) {
+  socket := abkzeromq.ZmqRep(req_port, rep_port)
+  for {
+    msg, _ := socket.Recv(0)
+    msg_arr := strings.Fields(string(msg))
+    _axn, _type, _key := msg_arr[0], msg_arr[1], msg_arr[2]
+    return_value := ""
 
-func DeleteKey(key string) bool{
-  return DelKey(key)
+    if _axn == "read" {
+      _get_val := GetValTask(_type)
+      return_value = _get_val(_key)
+
+    } else if _axn == "push" {
+
+      if _type == "tsds" {
+        year, _ := strconv.Atoi(msg_arr[3])
+        month, _ := strconv.Atoi(msg_arr[4])
+        day, _ := strconv.Atoi(msg_arr[5])
+        hour, _ := strconv.Atoi(msg_arr[6])
+        min, _ := strconv.Atoi(msg_arr[7])
+        sec, _ := strconv.Atoi(msg_arr[8])
+        _value := strings.Join(msg_arr[9:], " ")
+        if PushKeyValTSDS(_key, _value, year, month, day, hour, min, sec){
+          return_value = _value
+        }
+      } else {
+        _push_keyval := PushKeyValTask(_type)
+        _value := strings.Join(msg_arr[3:], " ")
+        if _push_keyval(_key, _value) {
+          return_value = _value
+        }
+      }
+
+    } else if _axn == "delete" {
+      _del_key := DelKeyTask(_type)
+      if _del_key(_key) {
+        return_value = _key
+      }
+
+    } else {
+      fmt.Printf("unhandled request sent: %s", msg)
+
+    }
+    socket.Send([]byte(return_value), 0)
+    fmt.Println("Got: [ ", string(msg), " ]; Sent: [ ", return_value, " ]")
+  }
 }
 
 func GoShareZMQ(req_port int, rep_port int){
   fmt.Printf("starting ZeroMQ REP/REQ at %d/%d\n", req_port, rep_port)
   runtime.GOMAXPROCS(runtime.NumCPU())
 
-  abkzeromq.ZmqRep(req_port, rep_port, ReadKey, PushKey, DeleteKey)
+  goShareZmqRep(req_port, rep_port)
 }
--- zeromq/zeromq.go
+++ zeromq/zeromq.go
@@ -6,38 +6,15 @@ import (
   zmq "github.com/alecthomas/gozmq"
 )
 
-type READ func(key string) string
-type PUSH func(key string, val string) bool
-type DELETE func(key string) bool
 
-func ZmqRep(req_port int, rep_port int, read READ, push PUSH, del DELETE) {
+func ZmqRep(req_port int, rep_port int) *zmq.Socket {
   context, _ := zmq.NewContext()
   socket, _ := context.NewSocket(zmq.REP)
   socket.Bind(fmt.Sprintf("tcp://127.0.0.1:%d", req_port))
   socket.Bind(fmt.Sprintf("tcp://127.0.0.1:%d", rep_port))
 
   fmt.Printf("ZMQ REQ/REP Daemon at port %d and %d\n", req_port, rep_port)
-  for {
-    msg, _ := socket.Recv(0)
-    msg_arr := strings.Fields(string(msg))
-    return_value := ""
-    if msg_arr[0] == "read" {
-      return_value = read(msg_arr[1])
-    } else if msg_arr[0] == "push" {
-      _value := strings.Join(msg_arr[2:], " ")
-      if push(msg_arr[1], _value) {
-        return_value = _value
-      }
-    } else if msg_arr[0] == "delete" {
-      if del(msg_arr[1]) {
-        return_value = msg_arr[1]
-      }
-    } else {
-      fmt.Printf("unhandled request sent: %s", msg)
-    }
-    socket.Send([]byte(return_value), 0)
-    fmt.Println("Got: [ ", string(msg), " ]; Sent: [ ", return_value, " ]")
-  }
+  return socket
 }
 
 func ZmqReq(req_port int, rep_port int, dat ...string) []byte{
