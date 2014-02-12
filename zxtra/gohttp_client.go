3f902004adaddd9029c9d5bf72fb4c4a039def6b
AbhishekKr <abhikumar163@gmail.com>
2014-02-12 07:14:50 +0530

    updated zxtra/gohttp_client.go to use NS and TSDS dbtasks

--- zxtra/gohttp_client.go
+++ zxtra/gohttp_client.go
@@ -12,17 +12,56 @@ var (
   httpport   = flag.Int("port", 9999, "what Socket PORT to connect")
 )
 
+/* KeyVal Support */
+
 func GetURL(host string, port int, key string) string{
-  return fmt.Sprintf("http://%s:%d/get?key='%s'", host, port, key)
+  return fmt.Sprintf("http://%s:%d/get?key=%s", host, port, key)
 }
 
 func PutURL(host string, port int, key string, val string) string{
-  return fmt.Sprintf("http://%s:%d/put?key='%s'&val='%s'",
+  return fmt.Sprintf("http://%s:%d/put?key=%s&val=%s",
                      host, port, key, val)
 }
 
 func DelURL(host string, port int, key string) string{
-  return fmt.Sprintf("http://%s:%d/del?key='%s'", host, port, key)
+  return fmt.Sprintf("http://%s:%d/del?key=%s", host, port, key)
+}
+
+
+/* KeyVal NS Support */
+
+func NSGetURL(host string, port int, key string) string{
+  return fmt.Sprintf("http://%s:%d/get?key=%s&type=ns", host, port, key)
+}
+
+func NSPutURL(host string, port int, key string, val string) string{
+  return fmt.Sprintf("http://%s:%d/put?key=%s&val=%s&type=ns",
+                     host, port, key, val)
+}
+
+func NSDelURL(host string, port int, key string) string{
+  return fmt.Sprintf("http://%s:%d/del?key=%s&type=ns", host, port, key)
+}
+
+
+/* KeyVal TSDS Support */
+
+func TSDSGetURL(host string, port int, key string) string{
+  return fmt.Sprintf("http://%s:%d/get?key=%s&type=tsds", host, port, key)
+}
+
+func TSDSPutURL(host string, port int, key, val, year, month, day, hr, min, sec string) string{
+  return fmt.Sprintf("http://%s:%d/put?key=%s&val=%s&type=tsds&year=%s&month=%s&day=%s&hour=%s&min=%s&sec=%s",
+                     host, port, key, val, year, month, day, hr, min, sec)
+}
+
+func NowTSDSPutURL(host string, port int, key string, val string) string{
+  return fmt.Sprintf("http://%s:%d/put?key=%s&val=%s&type=tsds-now",
+                     host, port, key, val)
+}
+
+func TSDSDelURL(host string, port int, key string) string{
+  return fmt.Sprintf("http://%s:%d/del?key=%s&type=tsds", host, port, key)
 }
 
 func HttpGet(url string) string{
@@ -33,30 +72,41 @@ func HttpGet(url string) string{
   defer resp.Body.Close()
 
   body, _ := ioutil.ReadAll(resp.Body)
+  fmt.Printf("Url: %s; with\n result:\n%s\n", url, string(body))
+  fmt.Println("///////////////////////////////////////////////")
   return string(body)
 }
 
 func main(){
   flag.Parse()
 
-  fmt.Println("Creating a Key: 'myname'")
-  resp := HttpGet(PutURL(*httphost, *httpport, "myname", "anon"))
-  fmt.Println("Response:", resp)
-  fmt.Println("Reading a Key: 'myname'")
-  resp = HttpGet(GetURL(*httphost, *httpport, "myname"))
-  fmt.Println("Response:", resp)
+  HttpGet(PutURL(*httphost, *httpport, "myname", "anon"))
+  HttpGet(GetURL(*httphost, *httpport, "myname"))
+  HttpGet(PutURL(*httphost, *httpport, "myname", "anonymous"))
+  HttpGet(GetURL(*httphost, *httpport, "myname"))
+  HttpGet(DelURL(*httphost, *httpport, "myname"))
+  HttpGet(GetURL(*httphost, *httpport, "myname"))
 
-  fmt.Println("Updating a Key: 'myname'")
-  resp = HttpGet(PutURL(*httphost, *httpport, "myname", "anonymous"))
-  fmt.Println("Response:", resp)
-  fmt.Println("Reading a Key: 'myname'")
-  resp = HttpGet(GetURL(*httphost, *httpport, "myname"))
-  fmt.Println("Response:", resp)
+  HttpGet(NSPutURL(*httphost, *httpport, "myname:last:first", "anon"))
+  HttpGet(NSGetURL(*httphost, *httpport, "myname:last:first"))
+  HttpGet(NSPutURL(*httphost, *httpport, "myname:last", "ymous"))
+  HttpGet(NSPutURL(*httphost, *httpport, "myname", "anonymous"))
+  HttpGet(NSGetURL(*httphost, *httpport, "myname:last"))
+  HttpGet(NSDelURL(*httphost, *httpport, "myname"))
+  HttpGet(NSGetURL(*httphost, *httpport, "myname:last"))
 
-  fmt.Println("Deleting a Key: 'myname'")
-  resp = HttpGet(DelURL(*httphost, *httpport, "myname"))
-  fmt.Println("Response:", resp)
-  fmt.Println("Reading a Key: 'myname'")
-  resp = HttpGet(GetURL(*httphost, *httpport, "myname"))
-  fmt.Println("Response:", resp)
+  HttpGet(NowTSDSPutURL(*httphost, *httpport, "myname:last:first", "anon"))
+  HttpGet(TSDSGetURL(*httphost, *httpport, "myname:last:first"))
+  HttpGet(TSDSPutURL(*httphost, *httpport, "myname:last", "ymous", "2014", "2", "10", "1", "1", "1"))
+  HttpGet(TSDSPutURL(*httphost, *httpport, "myname", "anonymous", "2014", "2", "10", "9", "8", "7"))
+  HttpGet(TSDSPutURL(*httphost, *httpport, "myname", "untitled", "2014", "2", "10", "0", "1", "7"))
+  HttpGet(TSDSGetURL(*httphost, *httpport, "myname:last"))
+  HttpGet(TSDSGetURL(*httphost, *httpport, "myname"))
+  HttpGet(TSDSGetURL(*httphost, *httpport, "myname:2014:February:10"))
+  HttpGet(TSDSDelURL(*httphost, *httpport, "myname"))
+  HttpGet(TSDSGetURL(*httphost, *httpport, "myname"))
+
+  HttpGet(NowTSDSPutURL(*httphost, *httpport, "myname:last:first", "anon"))
+  HttpGet(TSDSGetURL(*httphost, *httpport, "myname:last:first:2014:February"))
+  HttpGet(TSDSDelURL(*httphost, *httpport, "myname"))
 }
