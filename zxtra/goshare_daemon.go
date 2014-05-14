59ff975e199857b330341b3f16e3c69876cd9c31
AbhishekKr <abhikumar163@gmail.com>
2014-05-14 16:54:38 +0530

    zxtra now has a goshare_daemon for daemonized server and goshare_server

--- zxtra/goshare_daemon.go
+++ zxtra/goshare_daemon.go
@@ -1,7 +1,36 @@
 package main
 
-import "github.com/abhishekkr/goshare"
+import (
+	"flag"
+	"fmt"
 
-func main(){
-  goshare.GoShare()
+	"github.com/abhishekkr/goshare"
+
+	"github.com/ChaosCloud/godaemon"
+)
+
+var (
+	axn = flag.String("axn", "status", "status|start|stop")
+)
+
+func main() {
+	godaemon.MakeDaemon(&godaemon.DaemonAttr{})
+	flag.Parse()
+	switch *axn {
+	case "stop":
+		fmt.Println("Stopping...")
+		if godaemon.KillPID("") {
+			fmt.Println("Status: Stopped.")
+		} else {
+			fmt.Printf("Failed to stop. Status: ")
+			godaemon.StatusPID("")
+		}
+	case "status":
+		fmt.Printf("Status: ")
+		godaemon.StatusPID("")
+	default:
+		if godaemon.PersistPID("") {
+			goshare.GoShare()
+		}
+	}
 }
